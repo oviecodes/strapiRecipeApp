3ead4a03d2d43434c90ba765fa019d2d6c6b346c
Godwin Alexander <alecgee73@gmail.com>
2021-06-14 19:03:32 +0100

    production config set

--- config/database.js
+++ config/database.js
@@ -1,19 +1,67 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         srv: env.bool('DATABASE_SRV', false),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'recipe'),
+//         username: env('DATABASE_USERNAME', null),
+//         password: env('DATABASE_PASSWORD', null),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//     },
+//   },
+// });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'strapi'),
+//         username: env('DATABASE_USERNAME', 'strapi'),
+//         password: env('DATABASE_PASSWORD', 'strapi'),
+//         schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+//         ssl: {
+//           rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//         },
+//       },
+//       options: {},
+//     },
+//   },
+// });
+
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'recipe'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: true,
       },
     },
   },
